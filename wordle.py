1ddc92ea07b3649918491950e51f45bb1f3c6c30
xanavitate-dot <xanavitate@aimpa.org>
2026-02-19 09:35:29 -0500

    Implement user guess functionality in wordle.py
    Added user input for guessing a word and validation checks.

--- wordle.py
+++ wordle.py
@@ -1,6 +1,5 @@
 from pathlib import Path
-
-# Change 1: need to include random library
+# Change 1 : Something must be initialized here
 import random
 
 word_file = Path("5-letter-words.txt")
@@ -18,13 +17,26 @@ def load_word_bank (filename = "5-letter-words.txt"):
 	
 word_bank = load_word_bank()
 
+def user_guess(word_bank):
+	guess = input("Enter a 5 letter word: ").strip().lower()
+	# the strip function removes whitespace from strings
+	# the lower function makes all characters lowercase
+	if len(guess) != 5:
+		# != means not equals
+		print("You made a mistake, word submission must be 5 letters long. ")
+	elif guess not in word_bank:
+		# not in allows us to compare strings within arrays
+		print("That's not a real word. You should feel bad. ")
+	else:
+		return guess
+
 def main():
 	print("Wordle Test Project")
-	# Change 2: 
 	words = load_word_bank(word_file)
-	sample  = random.choice(words)
-	print(f"Test Sample Word: {sample}")
+	# Change 2 : Needs a way to call a random sample and print i
+	sample = random.choice(words)
+	guess = user_guess(words)
+	print(f"Your guess was {guess}, the word was {sample}")
 
-# Change 3: Need to run main function, so that is written below
 if __name__ == "__main__":
 	main()
